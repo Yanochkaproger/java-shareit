a7195d271fc7995c1021a7dc951d5ca8ede96f62
User Yana <ruavky4@yandex.ru>
2026-06-07 16:15:49 +0300

    Fix imports in server tests

--- server/src/test/java/ru/practicum/shareit/ItemRequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/ItemRequestControllerTest.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit;
 
+import ru.practicum.shareit.request.ItemRequestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
